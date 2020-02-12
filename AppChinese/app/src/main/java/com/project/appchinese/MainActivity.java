306f97af6ec27f9d215867981fad677884f4f6f7
Manu <ecousin12@gmail.com>
2020-02-12 15:16:17 +0100

    New activity_main.xml

--- AppChinese/app/src/main/java/com/project/appchinese/MainActivity.java
+++ AppChinese/app/src/main/java/com/project/appchinese/MainActivity.java
@@ -1,8 +1,11 @@
-<<<<<<< HEAD
 package com.project.appchinese;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.service.autofill.OnClickAction;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,20 +14,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-}
-=======
-package com.project.appchinese;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.os.Bundle;
-
-public class MainActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    public void OnClickNext(View view){
+        Intent intent =new Intent(this,Menu.class);
+        startActivity(intent);
     }
 }
->>>>>>> 96b544acf5e90454b5c55d75049fa74a8e706680
+
+
