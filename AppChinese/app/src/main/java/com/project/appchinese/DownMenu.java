a96cc9801fb44ea05fb7840699599fd8b2027f13
Manu <ecousin12@gmail.com>
2020-02-13 16:47:17 +0100

    gonna do the menu buttons with fragments

---
+++ AppChinese/app/src/main/java/com/project/appchinese/DownMenu.java
@@ -0,0 +1,31 @@
+package com.project.appchinese;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class DownMenu extends Fragment {
+
+
+    public DownMenu() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_down_menu, container, false);
+    }
+
+}
