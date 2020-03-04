15cb6555853618d18de3874d3e194f7049bb66b4
Manu <ecousin12@gmail.com>
2020-03-04 15:44:32 +0100

    BottomNavigationView not working

--- AppChinese/app/src/main/java/com/project/appchinese/MainActivity.java
+++ AppChinese/app/src/main/java/com/project/appchinese/MainActivity.java
@@ -1,10 +1,16 @@
 package com.project.appchinese;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
+
+import com.google.android.material.bottomnavigation.BottomNavigationItemView;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
 
     public void OnClickNext(View view){
         Intent intent =new Intent(this,Menu.class);
--- AppChinese/app/src/main/java/com/project/appchinese/Menu.java
+++ AppChinese/app/src/main/java/com/project/appchinese/Menu.java
@@ -1,14 +1,49 @@
 package com.project.appchinese;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.fragment.app.Fragment;
 import android.os.Bundle;
+import android.view.MenuItem;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class Menu extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_menu);//test
+        setContentView(R.layout.menu);
+
+        if(savedInstanceState == null){
+            getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout, new Learn()).commit();
+        }
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.neg_bar);
+        bottomNavigationView.setOnNavigationItemSelectedListener(navigationItemSelectedListener);
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navigationItemSelectedListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment fragment = null;
+
+                    switch (item.getItemId()) {
+                        case R.id.learn:
+                            fragment = new Learn();
+                            break;
+
+                        case R.id.dashboard:
+                            fragment = new Dashboard();
+                            break;
+
+                        case R.id.me:
+                            fragment = new Me();
+                            break;
+                    }
+
+                    getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout, fragment).commit();
+                    return true;
+                }
+            };
 }
