2d81ca0994131005c46557f1d8ec7682871ea205
JM REMEUR <jm@remeur.fr>
2020-03-05 09:22:11 +0100

    better like this :)

--- AppChinese/app/src/main/java/com/project/appchinese/Menu.java
+++ AppChinese/app/src/main/java/com/project/appchinese/Menu.java
@@ -7,43 +7,41 @@ import android.os.Bundle;
 import android.view.MenuItem;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class Menu extends AppCompatActivity {
-
-    private BottomNavigationView.OnNavigationItemSelectedListener navigationItemSelectedListener =
-            new BottomNavigationView.OnNavigationItemSelectedListener() {
-                @Override
-                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                    Fragment fragment = null;
-
-                    switch (item.getItemId()) {
-                        case R.id.learn:
-                            fragment = new Learn();
-                            break;
-
-                        case R.id.dashboard:
-                            fragment = new Dashboard();
-                            break;
-
-                        case R.id.me:
-                            fragment = new Me();
-                            break;
-                    }
-
-                    if(fragment != null){
-                        getSupportFragmentManager().beginTransaction().replace(R.id.content, fragment).commit();
-                    }
-                    return true;
-                }
-            };
-
+public class Menu extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener
+{
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.menu);
 
         getSupportFragmentManager().beginTransaction().replace(R.id.content, new Learn()).commit();
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.navigation);
-        bottomNavigationView.setOnNavigationItemSelectedListener(navigationItemSelectedListener);
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        Fragment fragment = null;
+
+        switch (item.getItemId()) {
+            case R.id.learn:
+                fragment = new Learn();
+                break;
+
+            case R.id.dashboard:
+                fragment = new Dashboard();
+                break;
+
+            case R.id.me:
+                fragment = new Me();
+                break;
+        }
+
+        if(fragment != null){
+            getSupportFragmentManager().beginTransaction().replace(R.id.content, fragment).commit();
+        }
+        return true;
     }
 }
