c11eabb30075d52bb701754c4eff8a035b71ab3b
Emmanuel <emmanuel@pop-os.localdomain>
2020-03-31 19:01:53 +0200

    ça passe

--- AppChinese/app/src/main/java/com/project/appchinese/MainActivity.java
+++ AppChinese/app/src/main/java/com/project/appchinese/MainActivity.java
@@ -1,6 +1,5 @@
 package com.project.appchinese;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -8,24 +7,8 @@ import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import android.os.Bundle;
-import android.text.TextUtils;
-import android.util.Log;
-import android.view.View;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
-import androidx.annotation.NonNull;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
