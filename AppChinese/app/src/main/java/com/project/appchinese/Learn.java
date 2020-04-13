63e75327a7b8fb9050c3f67740c1fccb4313583e
Blacksun1234 <ecousin12@gmail.com>
2020-04-13 17:57:27 +0200

    update

--- AppChinese/app/src/main/java/com/project/appchinese/Learn.java
+++ AppChinese/app/src/main/java/com/project/appchinese/Learn.java
@@ -4,6 +4,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
