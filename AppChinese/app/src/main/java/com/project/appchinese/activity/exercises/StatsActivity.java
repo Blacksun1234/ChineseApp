4a5112f10c5ac8ceaafa9367e6e6a730cf2edac2
Blacksun1234 <ecousin12@gmail.com>
2020-04-21 23:00:14 +0200

    StatsActivity.java

---
+++ AppChinese/app/src/main/java/com/project/appchinese/activity/exercises/StatsActivity.java
@@ -0,0 +1,31 @@
+package com.project.appchinese.activity.exercises;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+import com.project.appchinese.R;
+
+public class StatsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stats);
+
+        GraphView graph = (GraphView) findViewById(R.id.graph);
+        LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[] {
+                new DataPoint(0, 1),
+                new DataPoint(1, 0),
+                new DataPoint(2, 3)
+        });
+        graph.addSeries(series);
+    }
+
+    public void marks(){
+
+    }
+}
